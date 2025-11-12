00a47303ebc4e70789cfcd1d1413501ec2ebc5d6
Lim Xin Yin <xinyin050916@gmail.com>
2025-11-12 22:46:50 +0800

    testt

--- The Lucky Citizen Challenge/main1.java
+++ The Lucky Citizen Challenge/main1.java
@@ -1,21 +1,5 @@
 import java.util.Scanner;
-public class main {/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package vv;
-
-import java.util.Scanner;
-
-/**
- *
- * @author User
- */
-public class VV {
-
-    /**
-     * @param args the command line arguments
-     */
+public class main1 {
     public static void main(String[] args) {
           Scanner sc = new Scanner(System.in);
         
@@ -94,5 +78,4 @@ public class VV {
 }
 }
 
-
-}
+ 
